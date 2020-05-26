36372f2db5febbbf1bb14b98e51f3669833aa246
Ryid Nassif <ryid.nassif@gmail.com>
2020-05-26 16:30:22 +0100

    mapbox working, added cloudinary api

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,15 @@
 require("@rails/ujs").start()
+require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+// internal imports
+document.addEventListener('turbolinks:load', () => {
+  // Call your functions here, e.g:
+  // initSelect2();
+  initMapbox();
+});
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,8 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings, dependent: :destroy
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
