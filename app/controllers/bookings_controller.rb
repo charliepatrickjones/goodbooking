b70e5d51b475595c3ef4cfde4d39704e7cb3b126
Daichi Kondoh <d.kondo2618@gmail.com>
2020-05-27 22:05:25 +0100

    implement time slots (of three types)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,7 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+    @restaurants = Restaurant.all
+    render 'restaurants/index'
   end
 
   def show
@@ -19,20 +20,27 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @booking.restaurant = @restaurant
-    @booking.user = current_user
-    @booking.accepted = false # set default value
 
-    if @booking.save
-      redirect_to @restaurant
+    @remaining_slots = find_booking_slots
+    set_bookings
+
+    if @booking.party.nil? || @booking.time.nil?
+      render 'restaurants/show'
     else
-      render 'new'
+      if @booking.party > @remaining_slots[@booking.time]
+        flash.now[:alert] = "you can't book more than available seats"
+        render 'restaurants/show'
+      elsif @booking.save
+        redirect_to @restaurant, alert: 'your request has been sent!'
+        ## will update to redircet to user profile page
+      else
+        render 'restaurants/show'
+      end
     end
   end
 
   def update
     @booking = Booking.find(params[:id])
-
     if @booking.update(booking_params)
       redirect_to @booking
     else
@@ -43,12 +51,36 @@ class BookingsController < ApplicationController
   def destroy
     @booking = Booking.find(params[:id])
     @booking.destroy
-
     redirect_to bookings_path
   end
 
   private
-    def booking_params
-      params.require(:booking).permit(:date, :time, :party)
+
+  def booking_params
+    params.require(:booking).permit(:date, :time, :party)
+  end
+
+  def set_bookings
+    @booking.restaurant = @restaurant
+    @booking.user = current_user
+    @booking.accepted = false # set default value
+  end
+
+  def find_booking_slots
+    # convert string to array
+    opening_times = @restaurant.opening_time.split(",")
+    # set max capacity
+    capacity_h = opening_times.each_with_object({}) do |time, hash|
+      hash[time] = @restaurant.capacity
     end
+    # aggregate all sbookings of the restaurant
+    all_bookings = Booking.where(restaurant_id: @restaurant.id)
+    # subtract bookings time from max capacity
+    all_bookings.each do |booking|
+      capacity_h.each do |key, value|
+        capacity_h[key] -= booking.party if key == booking.time
+      end
+    end
+    capacity_h
+  end
 end
