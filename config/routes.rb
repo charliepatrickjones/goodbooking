4cfc50c04b06a908b4df67b3a8df9723a5a3f078
Daichi Kondoh <d.kondo2618@gmail.com>
2020-05-28 14:23:07 +0100

    create your bookings page, use faker in db:seed file

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :restaurants do
-    resources :reviews, only: [:show, :edit, :update, :new, :create, :index]
-    resources :bookings, only: [:show, :edit, :update, :new, :create, :index]
+    resources :reviews, only: [:show, :edit, :update, :new, :create]
+    resources :bookings, only: [:show, :edit, :update, :new, :create]
   end
-  resources :reviews, only: [:destroy]
-  resources :bookings, only: [:destroy]
+  resources :reviews, only: [:index,:destroy]
+  resources :bookings, only: [:index, :destroy]
 end
