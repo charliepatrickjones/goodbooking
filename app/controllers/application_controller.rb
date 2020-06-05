136f4b10bd4202de2f7205e9158c1133d230a68d
Daichi Kondoh <d.kondo2618@gmail.com>
2020-06-05 10:25:27 +0100

    set strong params for user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   add_flash_types :success, :warning, :danger, :info
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :name, :photo])
+  end
+
   def after_sign_in_path_for(resource)
     if current_user.role == 'guest'
       user_path(current_user)
